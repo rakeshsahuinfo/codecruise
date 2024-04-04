ea9cb6fa9d2a070796b8c751de6751c3dfd41286
sanjay <sanjaiswar@gmail.com>
2024-04-04 14:48:32 +0530

    c1

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\CourseModule;
 use App\Models\CourseTechStack;
+use Exception;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     public function index($id){
-        $course_id=decrypt($id);
-        $courseinfo=Course::find($course_id);
-        $coursemodule=CourseModule::where('course_id',$course_id)->first();
-        $coursetechstack=CourseTechStack::join('tech_stacks','tech_stacks.id','=','course_tech_stack.tech_stack_id')->where('course_tech_stack.course_id',$course_id)->select('tech_stacks.*')->get();
-        return view('course',['courseinfo'=>$courseinfo,'coursemodule'=>$coursemodule,'coursetechstack'=>$coursetechstack]);
+        try{
+            $course_id=decrypt($id);
+            $courseinfo=Course::find($course_id);
+            $coursemodule=CourseModule::where('course_id',$course_id)->first();
+            $coursetechstack=CourseTechStack::join('tech_stacks','tech_stacks.id','=','course_tech_stack.tech_stack_id')->where('course_tech_stack.course_id',$course_id)->select('tech_stacks.*')->get();
+            return view('course',['courseinfo'=>$courseinfo,'coursemodule'=>$coursemodule,'coursetechstack'=>$coursetechstack]);
+        }catch(Exception $e){
+            return back();
+        }
     }
 }

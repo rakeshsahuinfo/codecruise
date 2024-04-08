a4f5a78c516481100919df3ef685bd5bc853a5af
sanjay <sanjaiswar@gmail.com>
2024-04-08 14:23:05 +0530

    c1

--- resources/views/top-bar.blade.php
+++ resources/views/top-bar.blade.php
@@ -5,10 +5,14 @@
             <!--<i class="bi bi-phone-fill phone-icon num"></i> <span class="num"> (Prefer Email)</span>-->
         </div>
         <div class="social-links d-none d-md-block">
-            <a href="https://facebook.com/codecruise/" class="facebook" target="_new"><i class="bi bi-facebook"></i></a>
-            <a href="https://instagram.com/codecruise/" class="instagram" target="_new"><i class="bi bi-instagram"></i></a>
-            <a href="https://www.linkedin.com/company/codecruise" class="linkedin" target="_new"><i class="bi bi-linkedin"></i></i></a>
-            <a href="https://twitter.com/codecruiseindia/" class="twitter" target="_new"><i class="bi bi-twitter"></i></a>
+            <a href="https://facebook.com/codecruiseindia/" class="facebook" target="_new"><i
+                    class="bi bi-facebook"></i></a>
+            <a href="https://instagram.com/codecruise/" class="instagram" target="_new"><i
+                    class="bi bi-instagram"></i></a>
+            <a href="https://www.linkedin.com/company/codecruise" class="linkedin" target="_new"><i
+                    class="bi bi-linkedin"></i></i></a>
+            <a href="https://twitter.com/codecruiseindia/" class="twitter" target="_new"><i
+                    class="bi bi-twitter"></i></a>
         </div>
     </div>
 </section>

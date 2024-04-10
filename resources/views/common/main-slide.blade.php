252b736fc5a8085d3db5b009e4de8536a58134e1
sanjay <sanjaiswar@gmail.com>
2024-04-10 14:35:52 +0530

    c2

--- resources/views/common/main-slide.blade.php
+++ resources/views/common/main-slide.blade.php
@@ -1,137 +1,128 @@
-<div class="th-hero-wrapper hero-1" id="hero">
-    <div class="hero-slider-1 th-carousel" data-fade="true" data-slide-show="1" data-md-slide-show="1" data-dots="true">
-
-
-        <div class="th-hero-slide">
-            <div class="th-hero-bg" data-overlay="title" data-opacity="8" data-bg-src="{{asset('common/assets/img/slider/slide-11.png')}}">
-            </div>
-            <div class="container">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col-md-6">
-                        <div class="hero-style1">
-                            <span class="hero-subtitle" data-ani="slideinleft" data-ani-delay="0.1s"><span>35%
-                                    OFF</span> LEARN FROM TODAY</span>
-                            <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Education
-                                Is Create
-                                Better <span class="text-theme">Future.</span></h1>
-                            <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Education can be
-                                thought of as the transmission of the values and accumulated knowledge of a society.
-                                In this sense, it is equivalent.</p>
-                            <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
-                                <a href="contact.html" class="th-btn style3">Get Started<i
-                                        class="fas fa-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 text-lg-end text-center">
-                        <div class="hero-img1">
-                            <img src="{{asset('common/assets/img/hero/hero_thumb_1_1.jpg')}}" alt="hero">
+<div class="hero-slider-1 th-carousel" data-fade="true" data-slide-show="1" data-md-slide-show="1" data-dots="true">
+    <div class="th-hero-slide">
+        <div class="th-hero-bg" data-overlay="title" data-opacity="8"
+            data-bg-src="{{asset('common/assets/img/slider/slide-11.png')}}"></div>
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-6">
+                    <div class="hero-style1">
+                        <span class="hero-subtitle" data-ani="slideinleft"
+                            data-ani-delay="0.1s"><!-- <span>35% OFF</span>--> Anytime - Anywhere</span>
+                        <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Unlock your
+                            Potential <BR>
+                            Elevate your <span class="text-theme">Skills Now.</span></h1>
+                        <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Equip yourself with the skills
+                            and knowledge needed to thrive in an ever-evolving digital landscape.</p>
+                        <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
+                            <a href="#contact" class="th-btn style3">Get Started<i
+                                    class="fas fa-arrow-right ms-2"></i></a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="hero-shape shape1">
-                <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape2">
-                <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape3"></div>
-
-            <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
-                <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
-                <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
+                <div class="col-md-6 text-lg-end text-center">
+                    <div class="hero-img1">
+                        <img src="{{asset('common/assets/img/hero/hero_thumb_1_1.jpg')}}" alt="hero">
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="hero-shape shape1">
+            <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape2">
+            <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape3"></div>
 
+        <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
+            <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
+            <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
+        </div>
+    </div>
 
-        <div class="th-hero-slide">
-            <div class="th-hero-bg" data-overlay="title" data-opacity="8" data-bg-src="{{asset('common/assets/img/slider/slide-22.png')}}">
-            </div>
-            <div class="container">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col-md-6">
-                        <div class="hero-style1">
-                            <span class="hero-subtitle" data-ani="slideinleft" data-ani-delay="0.1s"><span>35%
-                                    OFF</span> LEARN FROM TODAY</span>
-                            <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Edura
-                                Leads To A Brighter <span class="text-theme">Future.</span></h1>
-                            <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Education can be
-                                thought of as the transmission of a societys values and accumulated knowledge. In
-                                this sense, it is equivalent.</p>
-                            <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
-                                <a href="contact.html" class="th-btn style3">Get Started<i
-                                        class="fas fa-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 text-lg-end text-center">
-                        <div class="hero-img1">
-                            <img src="{{asset('common/assets/img/hero/hero_thumb_1_2.jpg')}}" alt="hero">
+    <div class="th-hero-slide">
+        <div class="th-hero-bg" data-overlay="title" data-opacity="8"
+            data-bg-src="{{asset('common/assets/img/slider/slide-22.png')}}"></div>
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-6">
+                    <div class="hero-style1">
+                        <span class="hero-subtitle" data-ani="slideinleft" data-ani-delay="0.1s"> Anywhere -
+                            Online</span>
+                        <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Advanced learning
+                            solutions at <span class="text-theme">One Place.</span></h1>
+                        <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Stay ahead of the curve with
+                            cutting-edge training methodologies and industry-relevant curriculum. </p>
+                        <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
+                            <a href="#contact" class="th-btn style3">Get Started<i
+                                    class="fas fa-arrow-right ms-2"></i></a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="hero-shape shape1">
-                <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape2">
-                <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape3"></div>
-
-            <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
-                <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
-                <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
+                <div class="col-md-6 text-lg-end text-center">
+                    <div class="hero-img1">
+                        <img src="{{asset('common/assets/img/hero/hero_thumb_1_2.jpg')}}" alt="hero">
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="hero-shape shape1">
+            <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape2">
+            <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape3"></div>
 
+        <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
+            <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
+            <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
+        </div>
+    </div>
 
-        <div class="th-hero-slide">
-            <div class="th-hero-bg" data-overlay="title" data-opacity="8" data-bg-src="{{asset('common/assets/img/slider/slide-33.png')}}">
-            </div>
-            <div class="container">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col-md-6">
-                        <div class="hero-style1">
-                            <span class="hero-subtitle" data-ani="slideinleft" data-ani-delay="0.1s"><span>35%
-                                    OFF</span> LEARN FROM TODAY</span>
-                            <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">The
-                                Worlds Best Online Education <span class="text-theme">Institute.</span></h1>
-                            <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Education can be
-                                thought of as the transmission of the values and accumulated knowledge of a society.
-                                In this sense, it is equivalent.</p>
-                            <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
-                                <a href="contact.html" class="th-btn style3">Get Started<i
-                                        class="fas fa-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 text-lg-end text-center">
-                        <div class="hero-img1">
-                            <img src="{{asset('common/assets/img/hero/hero_thumb_1_3.jpg')}}" alt="hero">
+    <div class="th-hero-slide">
+        <div class="th-hero-bg" data-overlay="title" data-opacity="8"
+            data-bg-src="{{asset('common/assets/img/slider/slide-33.png')}}"></div>
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-6">
+                    <div class="hero-style1">
+                        <span class="hero-subtitle" data-ani="slideinleft"
+                            data-ani-delay="0.1s"><!-- <span>35% OFF</span> --> Anytime - Online</span>
+                        <h1 class="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Corporate and
+                            On-Campus <span class="text-theme">Training.</span></h1>
+                        <p class="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Empower your workforce with
+                            tailored training solutions designed to boost productivity and efficiency.</p>
+                        <div class="btn-group" data-ani="slideinleft" data-ani-delay="0.8s">
+                            <a href="#contact" class="th-btn style3">Get Started<i
+                                    class="fas fa-arrow-right ms-2"></i></a>
                         </div>
                     </div>
                 </div>
+                <div class="col-md-6 text-lg-end text-center">
+                    <div class="hero-img1">
+                        <img src="{{asset('common/assets/img/hero/hero_thumb_1_3.jpg')}}" alt="hero">
+                    </div>
+                </div>
             </div>
-            <div class="hero-shape shape1">
-                <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape2">
-                <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape3"></div>
+        </div>
+        <div class="hero-shape shape1">
+            <img src="{{asset('common/assets/img/hero/shape_1_1.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape2">
+            <img src="{{asset('common/assets/img/hero/shape_1_2.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape3"></div>
 
-            <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
-                <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
-            </div>
-            <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
-                <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
-            </div>
+        <div class="hero-shape shape4 shape-mockup jump-reverse" data-right="3%" data-bottom="7%">
+            <img src="{{asset('common/assets/img/hero/shape_1_3.png')}}" alt="shape">
+        </div>
+        <div class="hero-shape shape5 shape-mockup jump-reverse" data-left="0" data-bottom="0">
+            <img src="{{asset('common/assets/img/hero/shape_1_4.png')}}" alt="shape">
         </div>
     </div>
 </div>

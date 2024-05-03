f9d5c492ab7253c29815618001d3987f5a687860
sanjay <sanjaiswar@gmail.com>
2024-05-03 14:19:04 +0530

    c3

--- app/Http/Controllers/FeedbackAuthController.php
+++ app/Http/Controllers/FeedbackAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PromoSession;
 use App\Models\PromoSessionRegistration;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
@@ -13,29 +14,37 @@ class FeedbackAuthController extends Controller
 {
     public function redirectToGoogle($slug)
     {
-        $ps = PromoSession::where('slug', $slug)->first();
-        Session::put('promo_session_id', $ps->id);
-        return Socialite::driver('google')->redirect();
+        try {
+            $ps = PromoSession::where('slug', $slug)->first();
+            Session::put('promo_session_id', $ps->id);
+            return Socialite::driver('google')->redirect();
+        } catch (Exception $e) {
+            return redirect('/');
+        }
     }
 
     public function handleGoogleCallback()
     {
-        $user = Socialite::driver('google')->user();
-        $promo_session_id = Session::get('promo_session_id');
-        $ps = PromoSession::find($promo_session_id);
-        $psr = PromoSessionRegistration::where('promo_session_id', $promo_session_id)->where('email', $user->getEmail())->first();
-        if ($psr) {
-            $name = $psr->name;
-            $email = $psr->email;
-            $contact = $psr->contact;
-            $user_type = "registered";
-        } else {
-            $name = $user->getName();
-            $email = $user->getEmail();
-            $contact = "";
-            $user_type = "random";
+        try {
+            $user = Socialite::driver('google')->user();
+            $promo_session_id = Session::get('promo_session_id');
+            $ps = PromoSession::find($promo_session_id);
+            $psr = PromoSessionRegistration::where('promo_session_id', $promo_session_id)->where('email', $user->getEmail())->first();
+            if ($psr) {
+                $name = $psr->name;
+                $email = $psr->email;
+                $contact = $psr->contact;
+                $user_type = "registered";
+            } else {
+                $name = $user->getName();
+                $email = $user->getEmail();
+                $contact = "";
+                $user_type = "random";
+            }
+            //Log::info( $user->getEmail().'-'.$user->getName());
+            return view('common.feedback-for-promo-session', ['ps' => $ps, 'name' => $name, 'email' => $email, 'contact' => $contact, 'user_type' => $user_type]);
+        } catch (Exception $e) {
+            return redirect('/');
         }
-        //Log::info( $user->getEmail().'-'.$user->getName());
-        return view('common.feedback-for-promo-session', ['ps' => $ps, 'name' => $name, 'email' => $email, 'contact' => $contact, 'user_type' => $user_type]);
     }
 }
